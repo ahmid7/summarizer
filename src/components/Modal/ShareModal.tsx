58c51b1c1aff752dfe254d4dd59a86d733e7c336
ahmid sanni <ahmidsanni@gmail.com>
2023-01-31 13:39:18 +0100

    fixed sharemodal

--- src/components/Modal/ShareModal.tsx
+++ src/components/Modal/ShareModal.tsx
@@ -6,10 +6,9 @@ import {
     GrLinkedin, 
     GrReddit, 
     GrMail,
-    
 } from "react-icons/gr"
 import { AiOutlineCopy } from 'react-icons/ai'
-import gsap from "gsap/all"
+import toast from 'react-hot-toast'
 
 function ShareModal({ updateModal }: { updateModal: () => void }) {
 
@@ -17,6 +16,7 @@ function ShareModal({ updateModal }: { updateModal: () => void }) {
 
   function handleCopyText() {
     navigator.clipboard.writeText('https://summarizer-ajkfjmsxw-ahmid7.vercel.app/')
+    toast.success('Copied to clipboard')
   }
 
   React.useEffect(() => {
@@ -67,17 +67,17 @@ function ShareModal({ updateModal }: { updateModal: () => void }) {
   return (
     <div className='modal-wrapper divide-y-2 divide-gray-200 ' ref={ showModalRef }>
         <div className='flex justify-between items-center capitalize  font-merriweather py-2'>
-            <h4 className='text-[1.25vw]'>share modal</h4>
+            <h4 className='text-base md:text-[1.25vw]'>share modal</h4>
 
             <div 
                 onClick={ updateModal }
-                className='text-2xl cursor-pointer hover:text-coffee-bean-brown focus:text-coffee-bean-brown'
+                className='text-lg md:text-[1.67vw] cursor-pointer hover:text-coffee-bean-brown focus:text-coffee-bean-brown'
             >
                 <ImCancelCircle />
             </div>
         </div>
 
-        <div className='pt-4 text-[1.1vw]'>
+        <div className='pt-4 text-xs md:text-[1vw]'>
             <p>share this link via: </p>
 
             <div className='flex justify-between items-center py-3 iconContainer'>
@@ -89,7 +89,7 @@ function ShareModal({ updateModal }: { updateModal: () => void }) {
                                     href={ media.href }
                                     target='_blank'
                                     style={{ color: `${ media.color }` }} 
-                                    className='border-2 mediaIcon cursor-pointer border-gray-100 rounded-full h-14 w-14 text-xl flex items-center justify-center hover:border-[#bd6049] focus:border-[#bd6049]'
+                                    className='border-2 mediaIcon cursor-pointer border-gray-300 rounded-full h-10 w-10 md:h-[3.9vw] md:w-[3.9vw] text-lg md:text-[1.39vw] flex items-center justify-center hover:border-[#bd6049] focus:border-[#bd6049]'
                                 >
                                     { media.icon }
                                 </a>
@@ -101,17 +101,17 @@ function ShareModal({ updateModal }: { updateModal: () => void }) {
                 }
             </div>
 
-            <p className='capitalize text-[1vw] pb-4 py-3'>or copy link</p>
+            <p className='capitalize text-xs py-2 md:text-[0.9vw] md:py-[0.83vw]'>or copy link</p>
 
-            <div className=' relative z-20 overflow-hidden border-2 border-gray-400 '>
+            <div className=' relative z-20 overflow-hidden rounded-lg border-2 border-gray-400 '>
                 <input 
                     type='text' 
                     readOnly 
-                    className='w-full py-2 px-3 rounded-sm opacity-70' value={ 'https://summarizer-ajkfjmsxw-ahmid7.vercel.app/' }
+                    className='w-full py-2 px-3  outline-none rounded-sm opacity-70' value={ 'https://summarizer-ajkfjmsxw-ahmid7.vercel.app/' }
                 />
                 
                 <div 
-                    className='absolute top-0 right-0 text-3xl overflow-hidden cursor-pointer bg-white z-50 border-2 h-full flex items-center hover:text-coffee-bean-brown focus:text-coffee-bean-brown'
+                    className='absolute top-0 right-0 text-xl px-1 md:px-0  md:text-[2.08vw] overflow-hidden cursor-pointer bg-white z-50 border-2 h-full flex items-center hover:text-coffee-bean-brown focus:text-coffee-bean-brown'
                     onClick={ handleCopyText }
                 >
                     <AiOutlineCopy />
